1c941ee63ef58b246eb5bbe953043b80ab798bb4
katya_puzan <katesamek357@gmail.com>
2022-01-08 11:53:04 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+    const str = String(Math.abs(n));
+    let result = '';
+    let i = str.length - 1;
+    while (i >= 0) {
+        result = `${result}${str[i]}`;
+        i -= 1;
+    }
+    return Number(result);
+  }
   
-}
